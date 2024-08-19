e8f775d6528fb762daf6c2154e709b7d1dab1756
vunh <vunh@nobisoft.com.vn>
2024-08-19 13:42:25 +0700

    first commit

--- App.tsx
+++ App.tsx
@@ -1,117 +1,46 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
-  View,
 } from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+import MapView, {MarkerAnimated, UrlTile} from 'react-native-maps';
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+        <MapView
+            // style={styles.map}
+            style={{ flex: 1 }}
+            mapType="none"
+            // provider={null}
+            showsUserLocation={true}
+            showsTraffic={true}
+            userLocationCalloutEnabled
+            initialRegion={{
+              latitude: 21.030643,
+              longitude: 105.780387,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+        >
+          <UrlTile  zIndex={-1}  urlTemplate={'https://tiles.stadiamaps.com/styles/osm_bright.json?api_key=$f6b00bdc-e9cd-4905-843d-de79c055e203'}  />
+          {/*<SafeAreaView >*/}
+          {/*<Text>Maker</Text>*/}
+          <MarkerAnimated
+              coordinate={{latitude: 21.030643, longitude:105.780387 }}
+          />
+
+          <MarkerAnimated
+              coordinate={{latitude: 21.029311, longitude: 105.780114 }}
+          />
+          {/*</SafeAreaView>*/}
+        </MapView>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  map: {
+    ...StyleSheet.absoluteFillObject,
   },
 });
 
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,4 @@
 module.exports = {
   preset: 'react-native',
+  setupFilesAfterEnv: ["@react-native-mapbox-gl/maps/setup-jest"]
 };
